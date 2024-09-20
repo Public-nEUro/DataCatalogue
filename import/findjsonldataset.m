2ddcc45922eb5950570cd0c0835968d75934b46f
CPernet <wamcyril@gmail.com>
2024-09-20 10:48:51 +0200

    spm5+

--- import/findjsonldataset.m
+++ import/findjsonldataset.m
@@ -1,14 +1,16 @@
-function findjsonldataset(folder)
+function dataset_file = findjsonldataset(folder)
 
 % silly routine to find "type": "dataset" allowing to manually edit stuff
 
+dataset_file = 'not found';
 allfolders = dir(folder);
 for f= 3:size(allfolders,1)
     sub = dir(fullfile(allfolders(f).folder,allfolders(f).name));
     for s=3:size(sub,1)
         out = jsondecode(fileread(fullfile(sub(s).folder,sub(s).name)));
         if strcmpi(out.type,"dataset")
-            warning('check file %s',fullfile(sub(s).folder,sub(s).name))
+            dataset_file = fullfile(sub(s).folder,sub(s).name);
+            warning('dataset file %s',dataset_file)
         end
     end
 end
